d59847e36f4dfac2622449314a7f1580026ecbe1
Jacqueline Omollo <jacquelineOmolloo@gmail.com>
2019-08-02 00:17:06 -0400

    commit working out bug

--- src/components/NasaCard.js
+++ src/components/NasaCard.js
@@ -1,30 +1,28 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import NasaPics from "./NasaPics";
-import { tsPropertySignature } from "@babel/types";
 
 function NasaCard() {
-  const [nasaData, setData] = useState([]);
+  const [nasaData, setData] = useState("");
 
   useEffect(() => {
-    axios
-      .get(
-        "https://api.nasa.gov/planetary/apod?api_key=r4VDak3cdRd08LcBUSTyIPtq4BW1YTXUyKEPMjMy&date=2012-03-14"
-      )
-      .then(response => {
-        console.log(response.data);
-        setData(response.data);
-      })
+    axios.get(
+      "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY"
 
-      .catch(error => {
-        console.log("error: There was an error while getting data");
-      });
+        .then(response => {
+          console.log(response.data);
+          setData(response.data);
+          // console.log(response);
+        })
+
+        .catch(err => console.log(err))
+    );
+
     console.log(nasaData);
   }, [nasaData]);
 
   return (
     <div className="cardContainer">
-      <h4>The sky is the limit</h4>
       <NasaPics
         title={nasaData.title}
         date={nasaData.date}
